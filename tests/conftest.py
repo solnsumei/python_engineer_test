b8bce2cc079ecf710bb3d1830925ce44ea133ffd
Solomon Nsumei <solnsumei@gmail.com>
2023-01-25 11:52:54 +0100

    add failing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 def fetch_file_data(json_file: str) -> dict:
     with open(json_file) as f:
         data = json.load(f)
-        return data
+        return data['message']
 
 
 @pytest.fixture
@@ -17,3 +17,13 @@ def fetch_data1():
 @pytest.fixture
 def fetch_data2():
     return fetch_file_data("data/data_2.json")
+
+
+@pytest.fixture
+def fetch_result1():
+    return fetch_file_data("data/result_1.json")
+
+
+@pytest.fixture
+def fetch_result2():
+    return fetch_file_data("data/result_2.json")
--- tests/test_map_field_func.py
+++ tests/test_map_field_func.py
@@ -1,1 +1,43 @@
 import pytest
+from solution import map_field
+from solution import FieldType
+
+
+def test_str_fields(fetch_data1):
+    result = map_field(fetch_data1['battle']['id'])
+    assert type(result) == dict
+    assert result['type'] == FieldType.STRING
+    assert 'tag' in result
+    assert 'description' in result
+    assert result['required'] is False
+
+
+def test_integer_fields(fetch_data2):
+    result = map_field(fetch_data2['time'])
+    assert type(result) == dict
+    assert result['type'] == FieldType.INTEGER
+
+
+def test_boolean_fields(fetch_data2):
+    result = map_field(fetch_data2['publicFeed'])
+    assert result['type'] == FieldType.BOOLEAN
+
+
+def test_string_array_fields(fetch_data2):
+    result = map_field(fetch_data2['internationalCountries'])
+    assert result['type'] == FieldType.ENUM
+
+
+def test_object_array_fields(fetch_data1):
+    result = map_field(fetch_data1['battle']['participants'])
+    assert type(result) == dict
+    assert result['type'] == FieldType.ARRAY
+    assert 'items' in result
+
+
+def test_object_fields(fetch_data2):
+    result = map_field(fetch_data2['user'])
+    assert type(result) == dict
+    assert result['type'] == FieldType.OBJECT
+    assert 'schema' in result
+    assert type(result['schema']) == dict
