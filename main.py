99cc61a61600252987e626c3735fdf7ed03cfde8
Solomon Nsumei <solnsumei@gmail.com>
2023-01-25 12:43:54 +0100

    add method to run the program in main

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from solution import generate_schema_from_file
+
+
+def run_program():
+    source_file = input("Enter source file: ")
+    output_file = input("Enter output filename without extension: ")
+    saved_schema_file = generate_schema_from_file(source_file=source_file, output_file=output_file)
+    print(saved_schema_file)
+
+
 if __name__ == '__main__':
     print("Running program")
+    run_program()
